8d6282926f19bcef931b64525b1c19cc16d47714
Gabor Kotel <gabor.kotel@gmail.com>
2026-05-17 15:21:39 +0200

    feat(schema): add user schema and DTO for validation

--- shared/schema/src/lib/schema.ts
+++ shared/schema/src/lib/schema.ts
@@ -1,3 +1,18 @@
-export function schema(): string {
-  return 'schema';
-}
+import { z } from 'zod';
+
+export const BaseUserSchema = z.object({
+  id: z.string().min(5),
+  email: z.email(),
+});
+export type BaseUserDto = z.infer<typeof BaseUserSchema>;
+
+export const UserSchema = BaseUserSchema.extend({
+  password: z.string().min(5),
+});
+export type UserDto = z.infer<typeof UserSchema>;
+
+export const LoginCredentialsSchema = z.object({
+  email: z.email(),
+  password: z.string().min(5),
+});
+export type LoginCredentialsDto = z.infer<typeof LoginCredentialsSchema>;
