ec8f61a7b66cc0ef38df21820ce9197f52fd9105
Gabor Kotel <gabor.kotel@gmail.com>
2026-05-17 16:22:20 +0200

    feat(controller): implement login endpoint with Zod validation

--- backend/src/app/app.controller.ts
+++ backend/src/app/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import {
+  LoginCredentialsSchema,
+  BaseUserDto,
+  LoginCredentialsDto,
+} from '@one-validator-to-rule-them-all/shared/schema';
+import { ZodBody } from '../app/core';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getData() {
-    return this.appService.getData();
+  @Post('login')
+  login(
+    @ZodBody(LoginCredentialsSchema) loginDto: LoginCredentialsDto,
+  ): BaseUserDto {
+    return this.appService.login(loginDto);
   }
 }
--- backend/src/app/app.service.ts
+++ backend/src/app/app.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import {
+  BaseUserDto,
+  LoginCredentialsDto,
+} from '@one-validator-to-rule-them-all/shared/schema';
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Hello API' };
+  login(loginBody: LoginCredentialsDto): BaseUserDto {
+    const testUser = {
+      id: 'user_123',
+      password: 'test123',
+      email: 'admin@example.com',
+    };
+    if (
+      loginBody.email !== testUser.email ||
+      loginBody.password !== testUser.password
+    ) {
+      throw new Error('Invalid credentials');
+    }
+
+    return { id: testUser.id, email: testUser.email };
   }
 }
